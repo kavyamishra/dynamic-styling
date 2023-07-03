196fc1476821e1ffa647739cd8b34afd2e69c6dd
Kavya <kvymishra@gmail.com>
2023-07-03 11:53:15 +0530

    Add css for button and added animations keyframes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('');
+  const [textColor, setTextColor] = useState('');
+  const [fontSize, setFontSize] = useState('');
+
+  const handleBackgroundColorChange = (e) => {
+    setBackgroundColor(e.target.value);
+  };
+
+  const handleTextColorChange = (e) => {
+    setTextColor(e.target.value);
+  };
+
+  const handleFontSizeChange = (e) => {
+    setFontSize(e.target.value + 'px');
+  };
+
+  const pageStyle = {
+    backgroundColor: backgroundColor,
+    color: textColor,
+    fontSize: fontSize,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={pageStyle}>
+      <h1>Change Page Style</h1>
+      <div>
+        <label htmlFor="backgroundColor">Background Color:</label>
+        <input type="color" id="backgroundColor" onChange={handleBackgroundColorChange} />
+
+        <label htmlFor="textColor">Text Color:</label>
+        <input type="color" id="textColor" onChange={handleTextColorChange} />
+
+        <label htmlFor="fontSize">Font Size:</label>
+        <input type="number" id="fontSize" min="12" max="48" onChange={handleFontSizeChange} />
+      </div>
+      <p>Welcome to the styled page!</p>
     </div>
   );
 }
